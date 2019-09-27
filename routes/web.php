7167056b0b75d79658766f92c11c221916a6ad48
juan camacho <programascamacho@gmail.com>
2019-09-26 21:33:59 -0300

    prueba rutas web

--- routes/web.php
+++ routes/web.php
@@ -39,32 +39,19 @@ http: midominio.com/Carrito
 Route::get('/index', "PagesController@home");
 Route::get('/faqs', "PagesController@faqs");
 Route::get('/contacto', "PagesController@contact");
-<<<<<<< HEAD
-Route::get('/login', "PagesController@login");
-Route::get('/registro', "PagesController@register");
-Route::get('/admin', "PagesController@admin");
-
 Route::get('/familias', "FamiliesController@index");
-
-
-
-=======
-Route::get('/familias', "FamiliesController@index");
-Route::get('/register', "UsersController@register");
-Route::post('/register', "UsersController@register");
-Route::get('/admin', "PagesController@admin");
 
 ///  Usuarios Login /////
+Route::get('/register', "UsersController@register");
+Route::post('/register', "UsersController@register");
 Route::post('/login', "UsersController@login");
 Route::get('/login_page','UsersController@index');
 Route::post('/register_user','UsersController@register');
 Route::get('/login', "UsersController@login");
 Route::post('/user_login','UsersController@login');
 Route::get('/logout','UsersController@logout');
->>>>>>> 637b7548660005d6679b35a0a85ea1d4bfad3628
 
-/*--Panel y paginas de Admin--*/
-
+Route::get('/admin', "PagesController@admin");
 
 /*-- CREO RECORDAR QUE ACA HAY QUE HACE RUTA DOBLE, DE GET Y POST PERO NO ME ACUERDO ASI QUE LAS DEJO A MEDIO HACER -- Agustina --
 Route::get('/admin/familias', function () {return view('pages.admin.families');});
