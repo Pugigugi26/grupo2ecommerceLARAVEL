153ac872796a020903a60a22d99238758ae6c6e3
juan camacho <programascamacho@gmail.com>
2019-09-30 02:20:48 -0300

    barra de navegacion dinamica

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -1,28 +1,31 @@
-@extends('layouts.app')
-
+@extends('layouts.front.websitestructure')
+@section ('title', 'Verificacion')
 @section('content')
+    <main>
+
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Verify Your Email Address') }}</div>
+                <div class="card-header">{{ __('Verifica tu Email!') }}</div>
 
                 <div class="card-body">
                     @if (session('resent'))
                         <div class="alert alert-success" role="alert">
-                            {{ __('A fresh verification link has been sent to your email address.') }}
+                            {{ __('Un nuevo Link de verificacion se envio a tu Mail!') }}
                         </div>
                     @endif
 
-                    {{ __('Before proceeding, please check your email for a verification link.') }}
-                    {{ __('If you did not receive the email') }},
+                    {{ __('Por favor verifica tu Email el link enviado!') }}
+                    {{ __('No te LLego el Email?') }},
                     <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
                         @csrf
-		                <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('click here to request another') }}</button>.
+		                <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('Haz click aqui para pedir otra!') }}</button>.
 	                </form>
                 </div>
             </div>
         </div>
     </div>
 </div>
+</main>
 @endsection
--- resources/views/layouts/front/header.blade.php
+++ resources/views/layouts/front/header.blade.php
@@ -6,36 +6,46 @@
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link" href="" >HOME</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="familias">PRODUCTOS</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="">NOSOTROS</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="faqs">FAQS</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="contacto">CONTACTO</a>
-                </li>
+                <li class="nav-item"><a class="nav-link" href="" >HOME</a></li>
+                <li class="nav-item"><a class="nav-link" href="familias">PRODUCTOS</a></li>
+                <li class="nav-item"><a class="nav-link" href="">NOSOTROS</a></li>
+                <li class="nav-item"><a class="nav-link" href="faqs">FAQS</a></li>
+                <li class="nav-item"><a class="nav-link" href="contacto">CONTACTO</a></li>
             </ul>
             <ul class="navbar-nav ml-auto">
-              <li class="nav-item">
-                  <a class="nav-link" href=""><i class="fa fa-shopping-cart" aria-hidden="true" title="Carrito"></i></a>
-              </li>
-                <li class="nav-item">
-              <a class="nav-link" href=""><i class="fas fa-power-off" aria-hidden="true" title="Salir"></i> </a>
-                </li>
+                <!-- Right Side Of Navbar -->
+                    <!-- Authentication Links -->
+                    @guest
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('login') }}">{{ __('INGRESO') }}</a>
+                        </li>
+                        @if (Route::has('register'))
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('register') }}">{{ __('REGISTRO') }}</a>
+                            </li>
+                        @endif
+                    @else
+                        <li class="nav-item"><a a class="nav-link" href="familias"><i class="fa fa-shopping-cart" aria-hidden="true" ></i></a>
+                        <li class="nav-item dropdown">
+                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                {{ Auth::user()->name }} <span class="caret"></span>
+                                <i class="fa fa-user"></i>
+                            </a>
 
-                <li class="nav-item">
-                  <a class="nav-link" href="login">INGRESA</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="register">REGISTRATE</a>
-                </li>
+                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="{{ route('logout') }}"
+                                   onclick="event.preventDefault();
+                                                 document.getElementById('logout-form').submit();">
+                                    {{ __('Logout') }}
+                                    <i class="fas fa-power-off" title="Salir"></i>
+                                </a>
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                    @csrf
+                                </form>
+                            </div>
+                        </li>
+                    @endguest
+
             </ul>
         </div>
     </nav>
